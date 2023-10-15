29e91d05eda83fd7fd932d56a4d66dadbca332b2
Bridgit Arika <bridgitmob@gmail.com>
2023-10-15 06:33:45 -0700

    final project

---
+++ src/main/java/com/myCapstone/VillaFinalCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.myCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillaFinalCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillaFinalCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myCapstone/VillaFinalCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillaFinalCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
